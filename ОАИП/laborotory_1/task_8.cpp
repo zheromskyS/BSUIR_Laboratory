b50c997ac2d7b0a96fd2c2311cec0d9f44b6396d
Stanislau Zheromski <stanislauzheromski@MacBook-Pro-Stanislau.local>
2026-09-04 13:13:07 +0300

    laborotory_1

---
+++ ОАИП/laborotory_1/task_8.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main() {
+    double x, y, z = 0;
+
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+    cout << "Enter z: ";
+    cin >> z;
+
+    cout << pow(M_E, fabs(x - y)) * pow(fabs(x - y), x + y) / (atan(x) + atan(z)) + cbrt(pow(x, 6) + pow(log(y), 2))<< endl;
+    return 0;
+}
